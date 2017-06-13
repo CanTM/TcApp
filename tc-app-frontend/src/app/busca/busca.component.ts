6930dd3a3bf70bb13b39d799a5f1022b9ba95945
Candice Tosi Michelon <candicem@dbserver.com.br>
2017-06-13 13:57:33 -0300

    Refatoração do front

--- tc-app-frontend/src/app/busca/busca.component.ts
+++ tc-app-frontend/src/app/busca/busca.component.ts
@@ -16,22 +16,17 @@ class Frequency {
 })
 
 export class BuscaComponent implements OnInit {
-
-  title = 'Pesquisa: Tweets';
-  subtitle = 'Número de Tweets';
   
   STATISTICS: Frequency[] = [];
   recorrente = true;
-  threadsCount = 0;
-  nroBarra = 1;
+  displayDate: string;
 
   constructor() {
   }
   
   ngOnInit() {
-    var date = new Date().toLocaleTimeString().split(":");
-    var displayDate = date[0] + ":" + date[1];
-    var freq: Frequency = {letter: displayDate, frequency: 0};
+    this.set_date_now();
+    var freq: Frequency = {letter: this.displayDate, frequency: 0};
     this.STATISTICS.push(freq);
     this.initSvg();  
     this.initAxis();
@@ -43,6 +38,11 @@ export class BuscaComponent implements OnInit {
     this.recorrente = true;
     this.STATISTICS.pop();
     this.get_data();
+  }
+
+  set_date_now(){
+    var date = new Date().toLocaleTimeString().split(":");
+    this.displayDate = date[0] + ":" + date[1];
   }
 
   get_data() {
@@ -59,16 +59,13 @@ export class BuscaComponent implements OnInit {
       .then(
       (data) => {
         let json = JSON.parse(data);
-        var date = new Date().toLocaleTimeString().split(":");
-        var displayDate = date[0] + ":" + date[1];
-        var freq: Frequency = {letter: displayDate, frequency: json.data};
+        this.set_date_now();
+        var freq: Frequency = {letter: this.displayDate, frequency: json.data};
         this.STATISTICS.push(freq);
         console.log("data " + data);
         this.STATISTICS.forEach(element => {
           console.log("frequency " + element.letter + " " + element.frequency);
         });
-        console.log("nroBarra " + this.nroBarra);
-        this.nroBarra++;
         this.desenhar();
         if (this.recorrente === true) {
           this.get_data();
@@ -89,44 +86,46 @@ export class BuscaComponent implements OnInit {
     this.drawBars();
   }
 
-  private width: number;
-  private height: number;
-  private margin = {top: 20, right: 20, bottom: 30, left: 40};
+//Grafico nro de tweets
 
-  private x: any;
-  private y: any;
-  private svg: any;
-  private g: any;
+  private width1: number;
+  private height1: number;
+  private margin1 = {top: 20, right: 20, bottom: 30, left: 40};
+
+  private x1: any;
+  private y1: any;
+  private svg1: any;
+  private g1: any;
 
 
   private initSvg() {
-    this.svg = d3.select("svg");
-    this.width = +this.svg.attr("width") - this.margin.left - this.margin.right + 40;
-    this.height = +this.svg.attr("height") - this.margin.top - this.margin.bottom;
-    this.g = this.svg.append("g")
-                     .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-    this.g.append("rect")
-          .attr("width", this.width + 40)
-          .attr("height",this.height + 40)
+    this.svg1 = d3.select("#chart").select("svg");
+    this.width1 = +this.svg1.attr("width") - this.margin1.left - this.margin1.right + 40;
+    this.height1 = +this.svg1.attr("height") - this.margin1.top - this.margin1.bottom;
+    this.g1 = this.svg1.append("g")
+                     .attr("transform", "translate(" + this.margin1.left + "," + this.margin1.top + ")");
+    this.g1.append("rect")
+          .attr("width", this.width1 + 40)
+          .attr("height",this.height1 + 40)
           .style("fill", "white");
   }
 
   private initAxis() {
-    this.x = d3Scale.scaleBand().rangeRound([0, this.width - 10]).padding(0.1);
-    this.y = d3Scale.scaleLinear().rangeRound([this.height, 0]);
-    this.x.domain(this.STATISTICS.map((d) => d.letter));
-    this.y.domain([0, d3Array.max(this.STATISTICS, (d) => d.frequency)]);
+    this.x1 = d3Scale.scaleBand().rangeRound([0, this.width1 - 40]).padding(0.1);
+    this.y1 = d3Scale.scaleLinear().rangeRound([this.height1, 0]);
+    this.x1.domain(this.STATISTICS.map((d) => d.letter));
+    this.y1.domain([0, d3Array.max(this.STATISTICS, (d) => d.frequency)]);
   }
 
   private drawAxis() {
-    this.g.append("g")
+    this.g1.append("g")
           .attr("class", "axis axis--x")
-          .attr("transform", "translate(40," + this.height + ")")
-          .call(d3Axis.axisBottom(this.x));
-    this.g.append("g")
+          .attr("transform", "translate(40," + this.height1 + ")")
+          .call(d3Axis.axisBottom(this.x1));
+    this.g1.append("g")
           .attr("class", "axis axis--y")
           .attr("transform", "translate(40," + 0 + ")")
-          .call(d3Axis.axisLeft(this.y))
+          .call(d3Axis.axisLeft(this.y1))
           .append("text")
           .attr("class", "axis-title")
           .attr("transform", "rotate(-90)")
@@ -137,15 +136,17 @@ export class BuscaComponent implements OnInit {
   }
 
   private drawBars() {
-    this.g.selectAll(".bar")
+    this.g1.selectAll(".bar")
           .data(this.STATISTICS)
           .enter().append("rect")
           .style("fill", "steelblue")
           .attr("transform", "translate(40," + 0 + ")")
-          .attr("x", (d) => this.x(d.letter) )
-          .attr("y", (d) => this.y(d.frequency) )
-          .attr("width", this.x.bandwidth())
-          .attr("height", (d) => this.height - this.y(d.frequency) );
+          .attr("x", (d) => this.x1(d.letter) )
+          .attr("y", (d) => this.y1(d.frequency) )
+          .attr("width", this.x1.bandwidth())
+          .attr("height", (d) => this.height1 - this.y1(d.frequency) );
   }
+
+  //Grafico stream
 
 }
